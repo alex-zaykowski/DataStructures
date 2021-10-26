6c8eb12fe7da102b67962b7f06578592cbab5b9c
alex-zaykowski <alexander.zaykowski@hws.edu>
2021-10-25 23:24:33 -0400

    Updated solution from palindrome linked list probelm

--- LinkedLists/palindrome_linked_list.py
+++ LinkedLists/palindrome_linked_list.py
@@ -9,40 +9,17 @@ class ListNode:
         self.next = next
 
 def isPalindrome(head):
-    revHead = head
-    revHead = reverse(revHead)
+    stack = []
+    dummy = head
     isPalindrome = True
-    while(revHead and head):
-        print(str(revHead.val) + "  " + str(head.val))
-        if(revHead.val != head.val):
+    while(dummy):
+        stack.append(dummy.val)
+        dummy = dummy.next
+    while(stack):
+        value = stack.pop()
+        if(value != head.val):
             isPalindrome = False
             break
         else:
-            revHead = revHead.next
-            head = head.next  
+            head = head.next
     return isPalindrome
-    
-def reverse(head):
-    prev = None
-    newHead = None
-    while(head):
-        newHead = head
-        nextNode = head.next
-        head.next = prev
-        prev = head
-        head = nextNode
-    return newHead
-
-
-def PrintList(node):
-    while(node):
-        print(node.val)
-        node = node.next
-
-def main():
-    head = ListNode(1)
-    head.next = ListNode(1)
-    head.next.next = ListNode(2)
-    head.next.next.next = ListNode(1)
-    print(isPalindrome(head))
-main()

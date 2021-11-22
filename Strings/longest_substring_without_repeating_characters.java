755d4e9b7c0d76141dc06d5109d084fefca371cd
alex-zaykowski <alexander.zaykowski@hws.edu>
2021-11-22 10:22:58 -0500

    updated solution

--- Strings/longest_substring_without_repeating_characters.java
+++ Strings/longest_substring_without_repeating_characters.java
@@ -1,3 +1,4 @@
+
 /**
  * This is my brute force solution to the longest substring 
  * without repeating characters problem, it's extraordinarily slow
@@ -5,23 +6,44 @@
  * 
  * I'm going to post an updated version with a faster run time along with this
  */
+import java.util.*;
+
 class Solution {
     public int lengthOfLongestSubstring(String s) {
         String totalString = "";
         String currString = "";
         int slow = 0;
-        for(int i = 0; i < s.length(); i++){
-            if(currString.indexOf(String.valueOf(s.charAt(i))) == -1){
+        for (int i = 0; i < s.length(); i++) {
+            if (currString.indexOf(String.valueOf(s.charAt(i))) == -1) {
                 currString += String.valueOf(s.charAt(i));
-                if(currString.length() > totalString.length()){
+                if (currString.length() > totalString.length()) {
                     totalString = currString;
                 }
-            }else{
+            } else {
                 slow += 1;
                 i = slow;
                 currString = String.valueOf(s.charAt(i));
             }
         }
         return totalString.length();
+    }
+
+    /**
+     * Second solution beating 24% of submissions in speed
+     */
+    public int lengthOfLongestSubstring2(String s) {
+        int max = 0;
+        HashSet<Character> chars = new HashSet<Character>();
+        for (int i = 0, j = 0; i < s.length(); i++) {
+            if (chars.contains(s.charAt(i))) {
+                chars.remove(s.charAt(j));
+                i--;
+                j++;
+            } else {
+                chars.add(s.charAt(i));
+                max = Math.max(chars.size(), max);
+            }
+        }
+        return max;
     }
 }
